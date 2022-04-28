9ce4e5fdef544a63edb665d12da791c9599486a4
ÐFarma20 <kustov.ius@students.dvfu.ru>
2022-04-28 15:19:04 +1000

    Доработан геймплей

--- QRPuzzel/main/static/js/GamePage.js
+++ QRPuzzel/main/static/js/GamePage.js
@@ -1,6 +1,6 @@
-var picture = new Map([["place_piece_1","1"], ["place_piece_2","2"], ["place_piece_3","3"],
-                ["place_piece_4","4"], ["place_piece_5","5"], ["place_piece_6","6"],
-                ["place_piece_7","7"], ["place_piece_8","8"], ["place_piece_9","9"], ])
+var picture = new Map([["place_piece_1","0"], ["place_piece_2","0"], ["place_piece_3","0"],
+                ["place_piece_4","0"], ["place_piece_5","0"], ["place_piece_6","0"],
+                ["place_piece_7","0"], ["place_piece_8","0"], ["place_piece_9","0"], ])
 
 document.addEventListener("click", function(div) {
   if (div.target.className=="place_piece") {
@@ -19,19 +19,28 @@ document.addEventListener("click", function(div) {
       picture.set(id_place, checked.id);
   }
 
-  if (div.target.parentElement.parentElement.parentElement.className=="puzzle-field"){
-      div.target.parentElement.querySelector('.piece_up').disabled = false;
-      document.getElementById('puzzle-pieces').appendChild(div.target.parentElement);
-      var id_place = div.target.parentElement.parentElement.id
-      picture.set(id_place, '0');
+  if (div.target.parentElement.parentElement.parentElement.parentElement.className=="puzzle-field"){
+      var id_place = div.target.parentElement.parentElement.parentElement.id
+      picture.set(id_place, "0");
+      div.target.parentElement.parentElement.querySelector('.piece_up').disabled = false;
+      document.getElementById('puzzle-pieces').appendChild(div.target.parentElement.parentElement);
+
   }
 
-  // if(div.target.className=="main" || div.target.className=="text" || div.target.className=="puzzle-subtitle"){
-  //     var radio = document.getElementsByClassName('piece_up');
-  //     for(var i = 0; i < radio.length; i++){
-  //         radio[i].checked = false;
-  //         console.log(1);
-  //     }
-  // }
+  if(div.target.className == "Myimg"){
+      div.target.parentElement.parentElement.querySelector('.piece_up').checked = true;
+  }
+
+
+  var flag = true;
+  for(var i = 1; i <= picture.size; i++){
+      if(picture.get('place_piece_'+i) != i){
+           flag = false;
+           break;
+      }
+  }
+  if(flag){
+      console.log('Вы выйграли');
+  }
 
 });
